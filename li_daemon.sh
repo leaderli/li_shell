2d5b353334f981cd46115d885fba632ad2ca4489
leaderli <429243408@qq.com>
2023-07-12 08:18:33 +0800

    守护进程

--- li_daemon.sh
+++ li_daemon.sh
@@ -1,19 +1,21 @@
 function daemon_pid() {
 
-  PID=$1
-  if [ -z "$PID" ]; then
-    echo "PID 未指定"
-    eixt 2
+  PID_FILE=$1
+  if [ -z "$PID_FILE" ]; then
+    echo "PID_FILE 未指定"
+    exit 2
   fi
 
   # 检测守护进程是否存在
-  if [ -f "$PID" ]; then
-    if kill -0 "$(cat "$PID")" 2>/dev/null; then
-      echo "守护进程已经存在"
+  if [ -f "$PID_FILE" ]; then
+    PID="$(cat $PID_FILE)"
+    if kill -0 "$PID" 2>/dev/null; then
+      echo "守护进程${PID}已经在运行"
       exit 1
     else
-      echo "守护进程已经失效，删除PID"
-      rm "$PID"
+      echo "守护进程${PID}未启动，删除${PID_FILE}"
+      rm "$PID_FILE"
     fi
   fi
+  echo $$ > "$PID_FILE"
 }
