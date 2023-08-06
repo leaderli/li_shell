3aa94c745d99bbeafdaad73e8cd3259078fe3b8d
leaderli <429243408@qq.com>
2023-08-06 19:00:20 +0800

    终止守护进程

--- li_daemon.sh
+++ li_daemon.sh
@@ -5,7 +5,7 @@ function start_daemon_by_pid_file() {
   PID_FILE=$1
   if [ -z "$PID_FILE" ]; then
     echo "PID_FILE 未指定"
-    exit 2
+    return 2
   fi
 
   # 检测守护进程是否存在
@@ -13,13 +13,12 @@ function start_daemon_by_pid_file() {
     PID="$(cat "$PID_FILE")"
     if kill -0 "$PID" 2>/dev/null; then
       echo "守护进程${PID}已经在运行"
-      exit 1
+      return 1
     else
       echo "守护进程${PID}未启动，删除${PID_FILE}"
       rm "$PID_FILE"
     fi
   fi
-  echo $$ >"$PID_FILE"
 }
 # 根据PID终止守护进程的执行
 # 参数：$1 保持守护进程PID的文件
@@ -28,7 +27,7 @@ function stop_daemon_by_pid_file() {
   PID_FILE=$1
   if [ -z "$PID_FILE" ]; then
     echo "PID_FILE 未指定"
-    exit 2
+    return 2
   fi
   # 检测守护进程是否存在
   if [ -f "$PID_FILE" ]; then
